790df88e722c234ebdda420e1fa346d5a5e9106e
ishala <faishalali22@students.amikom.ac.id>
2024-07-04 21:40:41 +0700

    adjustment on logging

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 from predict import predict_comment, tokenizer
+import logging
 
 app = Flask(__name__)
+
+# Konfigurasikan logging untuk debugging
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def index():
@@ -13,10 +17,16 @@ def prediction():
     categories = ['Netral', 'Positif', 'Negatif']  # Sesuaikan dengan kategori Anda
     pred_prob, predicted_category, pred_labels = predict_comment(tokenizer, comment, categories)
     
+    # Log hasil prediksi
+    logging.info(f"Comment: {comment}")
+    logging.info(f"Predicted Category: {predicted_category}")
+    logging.info(f"Prediction Probabilities: {pred_prob}")
+    logging.info(f"Prediction Labels: {pred_labels}")
+    
     # Setiap kategori diberi nilai 0 atau 1
-    netral = 1 if predicted_category == 0 else 0
-    positif = 1 if predicted_category == 1 else 0
-    negatif = 1 if predicted_category == 2 else 0
+    netral = 'True' if pred_labels == 0 else ''
+    positif = 'True' if pred_labels == 1 else ''
+    negatif = 'True' if pred_labels == 2 else ''
     
     # Tampilkan hasil prediksi
     return render_template('main.html', comment=comment, predicted_category=predicted_category, netral=netral, positif=positif, negatif=negatif)
